eb05a4ac005fab3a9c350a7dca20293865f5b5b0
RomanovCopy <RomanovCopy@gmail.com>
2023-04-09 18:18:30 +0300

    002end

---
+++ 002Truth_diagram/_002Truth_diagram.py
@@ -0,0 +1,8 @@
+﻿#Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z
+print("Таблица истинности для выражения ¬(X ∧ Y) ∨ Z")
+print("X\tY\tZ\tResult")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f"{x}\t{y}\t{z}\t{bool(not(x or y)and z )}")
+
