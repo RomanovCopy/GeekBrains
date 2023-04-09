aafeea774f2e39968e1f130fc64f493342b62e76
RomanovCopy <RomanovCopy@gmail.com>
2023-04-09 19:22:14 +0300

    003end

---
+++ 003Intersection_of_two_ines/_003Intersection_of_two_ines.py
@@ -0,0 +1,27 @@
+﻿#Даны две строки. Посчитайте сколько раз каждый символ первой строки встречается во второй
+
+
+#удаление повторов
+def res(listing):
+    length=len(listing)
+    for i in range(length):
+        for j in range( i+1, length):
+            if j<length and listing[i]==listing[j]:
+                listing.pop(j)
+    return listing
+
+
+substring=input("Подстрока : ")
+string=input("Строка : ")
+temp=[]
+#первичный подсчет
+for ss in substring:
+    c=0
+    for s in string:
+        if ss==s:
+            c+=1
+    temp.append((ss,c))
+print(res(temp))#выводим результат без повторов
+
+
+
