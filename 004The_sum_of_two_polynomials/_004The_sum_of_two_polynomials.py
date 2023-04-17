6d4d4c41ada594803dae49254739b7642839e434
RomanovCopy <RomanovCopy@gmail.com>
2023-04-17 21:36:05 +0300

    fix

--- 004The_sum_of_two_polynomials/_004The_sum_of_two_polynomials.py
+++ 004The_sum_of_two_polynomials/_004The_sum_of_two_polynomials.py
@@ -3,45 +3,65 @@
 # Найдите сумму данных многочленов
 
 
-
-
 def reading_data(path):
     with open(path, "r", encoding="utf-8") as file:
-        return file.readlines()
+        return file.readline()
 
-#разбиение многочлена на одночлены
-def division_into_monomials(polynomial):
-    set1={'+', '-'}
-    set2={'*', '/', '.','^'}
-    p = str(polynomial).replace('\n', '')
+
+# разбиение многочлена на одночлены
+def division_into_monomials2(polynomial):
+    p = str(polynomial).replace("\n", "")
     length = len(p)
     count = 0
-    mono = []
-    poly=[]
-    while count<length:
-        if p[count].isdigit() or p[count].isalpha() or len( set( p[count]).intersection(set2))>0:
-            mono.append( p[count])
-            if count==length-1:
-                poly.append(mono)
-        elif len( set( p[count]).intersection(set1))>0 or count==length-1:
-                poly.append(mono.copy())
-                poly.append(p[count])
-                mono.clear()
-        count+=1
-    return poly
+    i = 0
+    coeff = {}
+    var = {}
+    deg = {}
+    while i < (len(p)):
+        if p[i].isdigit() or p[i] == "^":
+            if p[i] == "^":
+                k = str("^")
+                i += 1
+            else:
+                k = str("")
+            j = i
+            while j < length and (p[j].isdigit() or p[j] == "."):
+                k += str(p[j])
+                j += 1
+            i = j
+            if k.__contains__("^"):
+                deg[count] = k
+            else:
+                coeff[count] = k
+            count += 1
+        elif p[i].isalpha():
+            j = i
+            k = str("")
+            while j < length and p[j].isalpha():
+                k += str(p[j])
+                j += 1
+            i = j
+            var[count] = k
+            count += 1
+        elif p[i] == "-" or p[i] == "+":
+            var[count] = p[i]
+            count += 1
+            i += 1
+        else:
+            i += 1
+    return coeff, var, deg
 
-#приведение подобных членов
-#def ghost_like_members(polynomial):
+
+# приведение подобных членов
+# def ghost_like_members(polynomial):
 #    last=""
 #    for mono in polynomial:
 #        for i in range(len(mono)):
 #            if mono[i].isdigit and last=='digital':
 
 
-
-
-
-polynomials = reading_data("polynomials.txt")
-polynom1 = division_into_monomials( polynomials[0])
-polynom2 = division_into_monomials(polynomials[1])
+polynomial1 = reading_data("polynomial1.txt")
+polynomial2 = reading_data("polynomial2.txt")
+polynom1 = division_into_monomials2(polynomial1)
+polynom2 = division_into_monomials2(polynomial2)
 print(f"{polynom1}\n{polynom2}")
