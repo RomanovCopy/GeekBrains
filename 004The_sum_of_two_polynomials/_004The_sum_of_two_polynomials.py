cdfeec520b9e8a02ab9a55acaac16f1b354b01ff
Сергей Романов <RomanovCopy@gmail.com>
2023-04-18 12:39:29 +0300

    fix

--- 004The_sum_of_two_polynomials/_004The_sum_of_two_polynomials.py
+++ 004The_sum_of_two_polynomials/_004The_sum_of_two_polynomials.py
@@ -1,72 +1,58 @@
 
 
-##считывание строки из файла
-#def reading_data(path):
-#    with open(path, "r", encoding="utf-8") as file:
-#        return file.readline().replace('\n','')
+#считывание строки из файла
+def reading_data(path):
+    with open(path, "r", encoding="utf-8") as file:
+        return file.readline().replace('\n','')
 
 #разбиение многочлена на одночлены
-#def division_into_monomials2(polynomial):
-#    p = str(polynomial).replace("\n", "")
-#    length = len(p)
-#    count = 0
-#    i = 0
-#    coeff = {}
-#    var = {}
-#    deg = {}
-#    while i < (len(p)):
-#        if p[i].isdigit() or p[i] == "^":
-#            if p[i] == "^":
-#                k = str("^")
-#                i += 1
-#            else:
-#                k = str("")
-#            j = i
-#            while j < length and (p[j].isdigit() or p[j] == "."):
-#                k += str(p[j])
-#                j += 1
-#            i = j
-#            if k.__contains__("^"):
-#                deg[count] = k
-#            else:
-#                coeff[count] = k
-#            count += 1
-#        elif p[i].isalpha():
-#            j = i
-#            k = str("")
-#            while j < length and p[j].isalpha():
-#                k += str(p[j])
-#                j += 1
-#            i = j
-#            var[count] = k
-#            count += 1
-#        elif p[i] == "-" or p[i] == "+":
-#            var[count] = p[i]
-#            count += 1
-#            i += 1
-#        else:
-#            i += 1
-#    return coeff, var, deg
-
-
-#def addition(poly1,poly2):
-#    length1=0
-#    for d in poly1:
-#        length1+=len(d)
-#    for i in range(length1):
-#        if i in poly1[0]:
-#            a=i
-
-
-    
-
-
-
-#polynomial1 = reading_data("polynomial1.txt")
-#polynomial2 = reading_data("polynomial2.txt")
-#polynom1 = division_into_monomials2(polynomial1)
-#polynom2 = division_into_monomials2(polynomial2)
-#print(f"{polynom1}\n{polynom2}")
+def division_into_monomials2(polynomial):
+    p = str(polynomial).replace("\n", "")
+    length = len(p)
+    count = 0
+    i = 0
+    coeff = {}
+    var = {}
+    deg = {}
+    num=False
+    while i < (len(p)):
+        if p[i].isdigit() or p[i] == "^":
+            if p[i] == "^":
+                k = str("^")
+                i += 1
+            else:
+                k = str("")
+                num=True
+            j = i
+            while j < length and (p[j].isdigit() or p[j] == "."):
+                k += str(p[j])
+                j += 1
+            i = j
+            if k.__contains__("^"):
+                deg[count] = k
+            else:
+                coeff[count] = k
+            count += 1
+        elif p[i].isalpha():
+            j = i
+            k = str("")
+            if num==False:
+                coeff[count]=1
+            else:
+                num=False
+            while j < length and p[j].isalpha():
+                k += str(p[j])
+                j += 1
+            i = j
+            var[count] = k
+            count += 1
+        elif p[i] == "-" or p[i] == "+":
+            var[count] = p[i]
+            count += 1
+            i += 1
+        else:
+            i += 1
+    return coeff, var, deg
 
 
 # coeffs - список коэффициентов многочлена
@@ -98,8 +84,21 @@ class Polynomial:
             else:
                 terms.append(f"{coeff}x^{i}")
         return " + ".join(terms)
+    
 
-p1=Polynomial([5, 3])#5x^2 + 3x создание первого многочлена
-p2=Polynomial([3, 1, 8])#2. 3x^2 + x + 8 создание второго многочлена
+polynomial1 = reading_data("polynomial1.txt")
+polynomial2 = reading_data("polynomial2.txt")
+polynom1 = division_into_monomials2(polynomial1)
+polynom2 = division_into_monomials2(polynomial2)
+koeff1=[]
+koeff2=[]
+for i in polynom1[0]:
+    koeff1.append(float(polynom1[0][i]))
+for i in polynom2[0]:
+    koeff2.append(float(polynom2[0][i]))
+
+
+p1=Polynomial(koeff1)#5x^2 + 3x создание первого многочлена
+p2=Polynomial(koeff2)#2. 3x^2 + x + 8 создание второго многочлена
 p3=p1+p2#сложение двух многочленов
 print(p3)#8x^2 + 4x + 8
