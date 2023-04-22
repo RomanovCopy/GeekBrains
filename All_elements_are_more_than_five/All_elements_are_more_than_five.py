ee0329f8af0aa3a0b861dc36a15b5b9b03f6d956
RomanovCopy <RomanovCopy@gmail.com>
2023-04-22 15:33:33 +0300

    fix

---
+++ All_elements_are_more_than_five/All_elements_are_more_than_five.py
@@ -0,0 +1,10 @@
+﻿#Задайте список случайных чисел от 1 до 10, выведите все элементы больше 5. 
+#Используйте для решения лямбда-функцию
+
+
+from random import randint
+
+s = int(input("Размер списка : "))
+v2=lambda: [randint(1, 11) for _ in range(s)]
+lst=[filter(v2 )]
+print(  )
