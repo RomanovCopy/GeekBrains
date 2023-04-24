0cc75e47d4d0847738accf7a1dd3b361b0178f6d
Сергей Романов <RomanovCopy@gmail.com>
2023-04-24 14:08:41 +0300

    fix

---
+++ 002/_002.py
@@ -0,0 +1,13 @@
+﻿#Даны два случайных пятизначных числа.
+#Определить состоят ли они из одинаковых цифр.
+
+
+a=int(input("number 1 :"))
+b=int(input("number 2 :"))
+
+d=True
+for c in str(a):
+    if c in str(b):
+        d=False
+        break
+print(d)
---
+++ 003/_003.py
@@ -0,0 +1,38 @@
+﻿#Напишите программу вычислени арифметического выражения 
+#заданного строкой.Используйте операции: + - / *
+#Приоритет операций стандартный.
+
+
+
+expression=input("Введите строку выражения : ")
+
+expression_elements={}
+element=[]
+last=0
+counter=0
+for i in range(len(expression)):
+    e=expression[i]
+    if e=='+' or e=='-':
+        element=[e,'o','sec',False]
+    elif e=='/' or e=='*':
+        element=[e,'o','first',False]
+    elif e=='=':
+        element=[e,'o','last',False]
+    else:
+        continue
+
+    v=float(expression[last:i])
+    expression_elements[counter]=[v,'num','' ,False]
+    counter+=1
+    expression_elements[counter]=element
+    counter+=1
+    last=i+1
+    if last==len(expression):
+        last=''
+     
+
+print(expression_elements)
+
+
+
+
