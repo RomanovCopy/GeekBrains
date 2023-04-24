d0a274465929e3f1681988230db7fb8c2bd16baa
RomanovCopy <RomanovCopy@gmail.com>
2023-04-24 19:53:35 +0300

    fix

---
+++ 003Simple_irreducible_fractions/_003Simple_irreducible_fractions.py
@@ -0,0 +1,14 @@
+﻿#Найдите все простые несократимые дроби, лежащие между 0 и 1, 
+#знаменатель которых не превышает 11.
+
+#есть ли общий делитель
+def is_common_divisor(numerator, denominator):
+    for i in range(2,12):
+        if numerator%i==0 and denominator%i==0:
+            return True
+    return False
+
+for i in range(1,12):
+    for j in range(i+1,12):
+        if not is_common_divisor(i, j):
+            print(f"{i}/{j}")
