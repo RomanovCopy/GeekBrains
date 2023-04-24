1b4935415b60d29b6b4201b57da5ac315cd6752c
Сергей Романов <RomanovCopy@gmail.com>
2023-04-24 15:03:10 +0300

    fix

--- 003/_003.py
+++ 003/_003.py
@@ -1,38 +1,39 @@
-﻿#Напишите программу вычислени арифметического выражения 
-#заданного строкой.Используйте операции: + - / *
-#Приоритет операций стандартный.
+﻿
+# Напишите программу вычислени арифметического выражения
+# заданного строкой.Используйте операции: + - / *
+# Приоритет операций стандартный.
+
+
+def expression_parsing(expression):
+    expression_elements = {}
+    element = []
+    last = 0
+    counter = 0
+    for i in range(len(expression)):
+        e = expression[i]
+        if e == "+" or e == "-":
+            element = [e, "o", "sec", False]
+        elif e == "/" or e == "*":
+            element = [e, "o", "first", False]
+        elif e == "=":
+            element = [e, "o", "last", False]
+        else:
+            continue
+        v = float(expression[last:i])
+        expression_elements[counter] = [v, "num", "", False]
+        counter += 1
+        expression_elements[counter] = element
+        counter += 1
+        last = i + 1
+    return expression_elements
+
+def calculation(expression_elements):
+    e=expression_elements
+    for key in e :
+        if e[key][1]=='o':
 
 
 
-expression=input("Введите строку выражения : ")
-
-expression_elements={}
-element=[]
-last=0
-counter=0
-for i in range(len(expression)):
-    e=expression[i]
-    if e=='+' or e=='-':
-        element=[e,'o','sec',False]
-    elif e=='/' or e=='*':
-        element=[e,'o','first',False]
-    elif e=='=':
-        element=[e,'o','last',False]
-    else:
-        continue
-
-    v=float(expression[last:i])
-    expression_elements[counter]=[v,'num','' ,False]
-    counter+=1
-    expression_elements[counter]=element
-    counter+=1
-    last=i+1
-    if last==len(expression):
-        last=''
-     
-
-print(expression_elements)
-
-
-
+expression = expression_parsing(input("Введите строку выражения : "))
+calculation(expression)
 
