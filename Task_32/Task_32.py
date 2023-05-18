9063729666beadbdbcc9581840bc5374291badd7
RomanovCopy <RomanovCopy@gmail.com>
2023-05-18 13:18:28 +0300

    fix

---
+++ Task_32/Task_32.py
@@ -0,0 +1,37 @@
+#Определить индексы элементов массива (списка),
+#значения которых принадлежат заданному диапазону (т.е. не
+#меньше заданного минимума и не больше заданного
+#максимума)
+
+
+from random import randint
+
+
+array=list(map(lambda x: randint(-100,100), range(20)))
+print(array)
+
+
+def find_indexes1(arr, min_val, max_val):
+    indexes = []
+    is_in_range = lambda x: min_val <= x <= max_val
+    for i in range(len(arr)):
+        if is_in_range(arr[i]):
+            indexes.append(i)
+    return indexes
+
+def find_indexes2(arr, min_val, max_val):
+    indexes = []
+    filtered_arr = list(filter(lambda x: min_val <= x <= max_val, arr))
+    for i in range(len(arr)):
+        if arr[i] in filtered_arr:
+            indexes.append(i)
+    return indexes
+
+min_val = 1
+max_val = 100
+indexes = find_indexes1(array, min_val, max_val)
+print(indexes) 
+
+
+indexes = find_indexes2(array, min_val, max_val)
+print(indexes)
