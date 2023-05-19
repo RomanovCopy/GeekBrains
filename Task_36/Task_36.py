05fff7d52e2d383e3dad0621547f186d190de9c4
RomanovCopy <RomanovCopy@gmail.com>
2023-05-19 18:38:08 +0300

    fix

--- Task_36/Task_36.py
+++ Task_36/Task_36.py
@@ -7,7 +7,12 @@
 
 
 from random import randint
-
+#Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+#которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+#столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+#которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+#почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+#ровно два аргумента, как, например, у операции умножения.
 
 def print_matrix(matrix):
     #печать нумерации столбцов
@@ -23,7 +28,7 @@ def print_matrix(matrix):
         print()
 
 
-def print_operation_table(operation, num_rows=6, num_columns=2):
+def print_operation_table(operation, num_rows=6, num_columns=6):
     #создание матрицы с заданными параметрами
     matrix = [[operation(i, j) for j in range(num_columns+1)[1:]] for i in range(num_rows+1)[1:]]
     #печать матрицы с нумерацией строк и столбцов
