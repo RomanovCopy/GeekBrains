7d4ef4a74dec49d968cbbc3980f42fb70481a4bb
RomanovCopy <RomanovCopy@gmail.com>
2023-05-20 18:24:02 +0300

    fix

--- An_analogue_of_the_Map_method/An_analogue_of_the_Map_method.py
+++ An_analogue_of_the_Map_method/An_analogue_of_the_Map_method.py
@@ -1,2 +1,14 @@
 #Создайте пользовательский аналог метода map()
+from random import randint
+
+#генератор случайного списка
+def create_iterable(length):
+    return [randint(1,100) for _ in range(length)]
+
+#аналог функции map()
+def custom_map(func, iterable):
+    return [ func(x) for x in iterable]
+
+#пример использования
+print(custom_map(lambda x: x*5, create_iterable(10)))
 
