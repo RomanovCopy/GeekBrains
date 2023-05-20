f112867b686d63ed75835117c32bb0c3f851518a
RomanovCopy <RomanovCopy@gmail.com>
2023-05-20 18:06:34 +0300

    fix

---
+++ Repeat_decorator/Repeat_decorator.py
@@ -0,0 +1,13 @@
+﻿#Создайте декоратор повторяющий функцию 
+#заданное количество раз.
+
+def decorator(func):
+    count=5
+    def dec(count):
+        for i in range(count):
+            func()
+
+
+@decorator
+def my_print():
+    print("Romanov")
