b92e4bd8c51b13966cf1fa40ac9404219756d095
RomanovCopy <RomanovCopy@gmail.com>
2023-05-22 19:11:19 +0300

    fix

---
+++ GeekBrainsPython/File_question_and_answer/File_question_and_answer.py
@@ -0,0 +1,12 @@
+﻿#Напишите программу, которая позволяет считывать из файла вопрос, отвечать на него и отправлять ответ обратно пользователю.
+import telebot
+import logging
+
+
+# создаем бота
+bot = telebot.TeleBot('TOKEN')
+
+# обработчик команды /start
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Здравствуйте! Какой у вас вопрос или проблема?')
--- GeekBrainsPython/Support_telebot/Support_telebot.py
+++ GeekBrainsPython/Support_telebot/Support_telebot.py
@@ -1,22 +1,31 @@
-﻿import telebot
-import logging
+﻿import datetime
+import telebot
 
 # создаем бота
 bot = telebot.TeleBot('TOKEN')
-
-# настраиваем логирование
-logging.basicConfig(filename='support.log', level=logging.INFO, format='%(asctime)s - %(message)s')
-
+#нумерация обращений
+index = 0
 # обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Здравствуйте! Какой у вас вопрос или проблема?')
-
+    #обнуляем нумерацию при старте бота
+    global index
+    index=0
 # обработчик всех сообщений
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=['text'])
 def handle_message(message):
+    global index
+    # Получение текущей даты и времени
+    now = datetime.datetime.now()
+    # Форматирование даты и времени в нужный формат
+    formatted_date = now.strftime('%d/%m/%Y/%H/%M/%S')
     # записываем обращение пользователя в файл
-    logging.info('[{} {}] {}'.format(message.from_user.first_name, message.from_user.last_name, message.text))
+    index+=1
+    text=f"{index}\n{message.chat.username}\n{formatted_date}\n{message.chat.id}\n{message.text}\n"
+    file=open('support.log','a', encoding='utf-8')
+    file.writelines(text)
+    file.close()
     bot.send_message(message.chat.id, 'Ваше обращение принято. Мы постараемся ответить вам как можно скорее.')
 
 # запускаем бота
