495acca13b7d1c800f78b639a1ce1101f7f88d89
RomanovCopy <RomanovCopy@gmail.com>
2023-05-22 21:09:23 +0300

    fix

--- GeekBrainsPython/File_question_and_answer/File_question_and_answer.py
+++ GeekBrainsPython/File_question_and_answer/File_question_and_answer.py
@@ -1,12 +1,46 @@
 ﻿#Напишите программу, которая позволяет считывать из файла вопрос, отвечать на него и отправлять ответ обратно пользователю.
 import telebot
-import logging
 
 
 # создаем бота
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('6052933328:AAFUc9gWc-nr9-_SkKH2qz0wn1nm-VfSbMY')
 
-# обработчик команды /start
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Здравствуйте! Какой у вас вопрос или проблема?')
+#открытие файла с сообщениями
+def open_file(path):
+    file=open(path, 'r', encoding='utf-8')
+    messages=file.readlines()
+    file.close()
+    return messages
+
+#отправка ответа
+def send_message(user_name, chat_id, date, message):
+    bot.send_message(chat_id, f" {user_name} на Ваш запрос от {date} отвечаем:\n\n{message}" )
+
+#работа с сообщениями
+def work_with_appeals(messages):
+    #array=str(messages).split('\n')
+    array=messages
+    #разбиваем список на отдельные списки содержащие в себе
+    #все данные по каждому запросу(по 4 строки)
+    #0 user_name
+    #1 дата в строчном виде
+    #2 chat_id
+    #3 текст сообщения
+    #это формат сохранения из предыдущего проекта "Support_telebot"
+    new_list = []
+    for i in range(0, len(array), 4):
+        new_list.append(array[i:i+4])
+    #работа оператора с обращениями
+    for user in new_list:
+        print(f"Имя: {user[0]}\nДата обращения: {user[1]}\n Chat Id пользователя: {user[2]}\nВопрос : {user[3]}\nЖду ответа :\n")
+        length=0
+        response=""
+        while length==0:
+            response=input()
+            length=len(response)
+        send_message(user[0],user[2],user[1],response)
+    print("Больше запросов нет.Спасибо.")
+
+work_with_appeals(open_file("support.log"))
+
+
--- GeekBrainsPython/Support_telebot/Support_telebot.py
+++ GeekBrainsPython/Support_telebot/Support_telebot.py
@@ -2,27 +2,20 @@
 import telebot
 
 # создаем бота
-bot = telebot.TeleBot('TOKEN')
-#нумерация обращений
-index = 0
+bot = telebot.TeleBot('6052933328:AAFUc9gWc-nr9-_SkKH2qz0wn1nm-VfSbMY')
 # обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Здравствуйте! Какой у вас вопрос или проблема?')
-    #обнуляем нумерацию при старте бота
-    global index
-    index=0
 # обработчик всех сообщений
 @bot.message_handler(content_types=['text'])
 def handle_message(message):
-    global index
     # Получение текущей даты и времени
     now = datetime.datetime.now()
     # Форматирование даты и времени в нужный формат
     formatted_date = now.strftime('%d/%m/%Y/%H/%M/%S')
     # записываем обращение пользователя в файл
-    index+=1
-    text=f"{index}\n{message.chat.username}\n{formatted_date}\n{message.chat.id}\n{message.text}\n"
+    text=f"{message.chat.username}\n{formatted_date}\n{message.chat.id}\n{message.text}\n"
     file=open('support.log','a', encoding='utf-8')
     file.writelines(text)
     file.close()
