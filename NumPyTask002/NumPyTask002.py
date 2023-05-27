527c55cd7767eba18df2e2e14693b9394a1b44c0
RomanovCopy <RomanovCopy@gmail.com>
2023-05-27 12:48:44 +0300

    fix

---
+++ NumPyTask002/NumPyTask002.py
@@ -0,0 +1,19 @@
+﻿import numpy as np
+
+
+size=10
+#массив случайных чисел от 0 до 5 из 10 элементов
+numbers=np.random.randint(10,100,size)
+print(numbers)
+
+#среднее арифметическое списка
+mean=np.mean(numbers)
+print(mean)
+
+#ближайшие по значению к числу 20 и его индекс
+
+dist=[np.abs(el-20) for el in numbers]
+print(dist)#список разниц
+minDist=np.min(dist)
+print(f"Минимальное расстояние: { minDist }")
+print(f"Индекс : {dist.index(minDist)}")
