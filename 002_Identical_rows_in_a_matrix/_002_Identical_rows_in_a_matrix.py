da075d7b172c4ca6782fe1e5b34bfabc8c0934a6
RomanovCopy <RomanovCopy@gmail.com>
2023-05-27 17:44:13 +0300

    fix

---
+++ 002_Identical_rows_in_a_matrix/_002_Identical_rows_in_a_matrix.py
@@ -0,0 +1,20 @@
+﻿#Создайте двумерный массив, размером 5х5. Определите, есть ли в нём одинаковые строки.
+
+import numpy as np
+
+# Создание двумерного массива размером 5х5
+matrix = np.random.randint(1, 10, size=(5, 5))
+
+print("Создана матрица:\n", matrix)
+
+#матрица из одинковых строк
+unique_rows = np.unique(matrix, axis=0)
+#уникальных строк
+unique_rows_len=unique_rows.shape[0]
+#строк в начальной матрице
+matrix_len=matrix.shape[0]
+#если unique_rows_len и matrix_len равны то одинаковых строк нет
+if unique_rows_len == matrix_len:
+    print("Одинаковых строк нет")
+else:
+    print("Найдены одинаковые строки")
