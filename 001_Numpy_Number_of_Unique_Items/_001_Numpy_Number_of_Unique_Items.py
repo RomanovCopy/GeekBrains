9222ef4cd9279b9f82ea0c61ec8562aa0e415e40
RomanovCopy <RomanovCopy@gmail.com>
2023-05-27 17:18:41 +0300

    fix

--- 001_Numpy_Number_of_Unique_Items/_001_Numpy_Number_of_Unique_Items.py
+++ 001_Numpy_Number_of_Unique_Items/_001_Numpy_Number_of_Unique_Items.py
@@ -2,11 +2,7 @@
 
 import numpy as np
 
-my_list = np.random.randint(1, 100, size=20)
-
-#unique_elements, counts = np.unique(my_list, return_counts=True)
+my_list = np.random.randint(1, 10, size=20)
 
 print(f"Создан список : {my_list}")
 print(f"Количество уникальных элементов : {len( np.unique(my_list))}", )
-#print("Количество каждого элемента:", counts)
-#print("Количество уникальных элементов:", len(unique_elements))
