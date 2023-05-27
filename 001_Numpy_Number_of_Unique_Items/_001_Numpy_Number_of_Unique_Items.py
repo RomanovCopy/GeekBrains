410dd99b500c3cb788ba1d124525d39e5054fa99
RomanovCopy <RomanovCopy@gmail.com>
2023-05-27 17:08:45 +0300

    fix

---
+++ 001_Numpy_Number_of_Unique_Items/_001_Numpy_Number_of_Unique_Items.py
@@ -0,0 +1,11 @@
+﻿#Дан список элементов. Используя библиотеку NumPy, подсчитайте количество уникальных элементов в нём.
+
+import numpy as np
+
+my_list = [1, 2, 3, 4, 2, 3, 1, 5, 6, 7, 8, 6]
+
+unique_elements, counts = np.unique(my_list, return_counts=True)
+
+print("Уникальные элементы:", unique_elements)
+print("Количество каждого элемента:", counts)
+print("Количество уникальных элементов:", len(unique_elements))
